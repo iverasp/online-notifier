7810901ed596afce6fd601b476b6f7e86c28853c
Iver Egge <iverasp@gmail.com>
2015-01-24 18:32:36 +0100

    example config

---
+++ example_config.py
@@ -0,0 +1,7 @@
+SQLALCHEMY_DATABASE_URI = 'sqlite3:///app.db'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'secret-key'
+RECAPTCHA_PUBLIC_KEY = 'secret'
+RECAPTCHA_PRIVATE_KEY = 'secret'
+RECAPTCHA_OPTIONS = {'theme': 'white'}
+STATIC_URL_PATH = '/notifier/static/'
